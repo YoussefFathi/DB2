6f3935b379d4661ade418c183bd61149bbc79af6
yousseffathi <youssef.fathy1164@gmail.com>
2019-02-24 18:38:26 +0200

    new

---
+++ src/Exceptions/DBAppException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class DBAppException extends Exception {
+
+}
---
+++ src/mashayet/Table.java
@@ -0,0 +1,13 @@
+package mashayet;
+
+import java.io.Serializable;
+
+public class Table implements Serializable {
+
+	public static void main(String[] args) {
+		Table new1 = new Table();
+		
+
+	}
+
+}
