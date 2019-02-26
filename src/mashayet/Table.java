fdfb171f867ae323c88fcc44e25d87c59ae1ae19
yousseffathi <youssef.fathy1164@gmail.com>
2019-02-26 12:43:02 +0200

    new

--- src/mashayet/Table.java
+++ src/mashayet/Table.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 public class Table implements Serializable {
 
+
+	private static final long serialVersionUID = 1L;
+
 	public static void main(String[] args) {
 		Table new1 = new Table();
 		
