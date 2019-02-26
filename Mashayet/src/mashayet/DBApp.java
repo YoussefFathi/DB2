3039bbd7df86fa3711e755b765f2cb9d82098e13
yousseffathi <youssef.fathy1164@gmail.com>
2019-02-26 18:24:13 +0200

    new

--- Mashayet/src/mashayet/DBApp.java
+++ Mashayet/src/mashayet/DBApp.java
@@ -1,28 +1,30 @@
 package mashayet;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
 
 import Exceptions.DBAppException;
 
 public class DBApp {
+	private ArrayList<Table> tables = new ArrayList();
 	public void init() {
 		// this does whatever initialization you would like
 	}
 
 	// or leave it empty if there is no code you want to
 	// execute at application startup
-	public static void createTable(String strTableName, String strClusteringKeyColumn,
+	public  void createTable(String strTableName, String strClusteringKeyColumn,
 			Hashtable<String, String> htblColNameType) throws DBAppException {
 		Table createdTable = new Table(strTableName, strClusteringKeyColumn, htblColNameType);
+		this.tables.add(createdTable);
 	}
 
 	public void createBitmapIndex(String strTableName, String strColName) throws DBAppException {
-
 	}
 
 	public void insertIntoTable(String strTableName, Hashtable<String, Object> htblColNameValue) throws DBAppException {
-
+		
 	}
 
 	public void updateTable(String strTableName, String strKey, Hashtable<String, Object> htblColNameValue)
@@ -45,8 +47,9 @@ public class DBApp {
 		htblColNameType.put("id", "java.lang.Integer");
 		htblColNameType.put("name", "java.lang.String");
 		htblColNameType.put("gpa", "java.lang.double");
+		DBApp app = new DBApp();
 		try {
-			createTable( strTableName, "id", htblColNameType );
+			app.createTable( strTableName, "id", htblColNameType );
 		} catch (DBAppException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- Mashayet/src/mashayet/Table.java
+++ Mashayet/src/mashayet/Table.java
@@ -15,58 +15,68 @@ public class Table implements Serializable {
 
 	// private static final long serialVersionUID = 1L;
 	transient private ArrayList pages = new ArrayList();
-	private String tableName="";
-	private  final int maxRows = 5;
+	private String tableName = "";
+	private final int maxRows = 5;
+	private int attrNo=0;
 	public Table(String tableName, String strClusteringKeyColumn, Hashtable<String, String> htblColNameType) {
-		this.tableName=tableName;
+		this.tableName = tableName;
 		try {
-			this.addToMeta(strClusteringKeyColumn,htblColNameType);
+			this.addToMeta(strClusteringKeyColumn, htblColNameType);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Page first = new Page();
-		pages.add((long) first.getID());
-		this.writePage(first);
+		Page second = new Page();
+		pages.add(0);
+		pages.add((int) (pages.get(pages.size() - 1)) + 1);
+		first.addTuple("this is the first tuple");
+		second.addTuple("this is the second tuple");
+		this.writePage(first, 0);
+		this.writePage(second, (int) (pages.get(pages.size() - 1)));
+		this.readPage(0);
+		this.readPage((int) (pages.get(pages.size() - 1)));
 	}
-	public void addToMeta(String key,Hashtable<String, String> table) throws IOException {
-		FileWriter writer= new FileWriter(new File("metaData.csv"));
+
+	public void addToMeta(String key, Hashtable<String, String> table) throws IOException {
+		FileWriter writer = new FileWriter(new File("metaData.csv"));
 		try {
 			writer.append("Table Name, Column Name, Column Type, Key,Indexed ");
 			writer.append('\n');
-			table.forEach((name,type)->{
+			table.forEach((name, type) -> {
 				try {
+					attrNo++;
 					writer.append(this.tableName);
 					writer.append(',');
 					writer.append(name);
 					writer.append(',');
 					writer.append(type);
 					writer.append(',');
-					if(key.equals(name)) {
+					if (key.equals(name)) {
 						writer.append("True");
-					}else {
+					} else {
 						writer.append("False");
 					}
 					writer.append(',');
 					writer.append("False");
 					writer.append('\n');
-					
+
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			});
 			System.out.println("Meta was created Successfully");
-		}
-		finally {
+		} finally {
 			writer.flush();
 			writer.close();
 		}
 	}
-	public void writePage(Page page) {
+
+	public void writePage(Page page, int indicator) {
 
 		try {
-			FileOutputStream fileOut = new FileOutputStream("Page.class");
+			FileOutputStream fileOut = new FileOutputStream("Page" + indicator + ".class");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(page);
 			out.close();
@@ -77,12 +87,13 @@ public class Table implements Serializable {
 		}
 	}
 
-	public Page readPage() {
+	public Page readPage(int indicator) {
 
 		try {
-			FileInputStream fileIn = new FileInputStream("Page.class");
+			FileInputStream fileIn = new FileInputStream("Page" + indicator + ".class");
 			ObjectInputStream in = new ObjectInputStream(fileIn);
 			Page e = (Page) in.readObject();
+			System.out.println(e.readTuples());
 			in.close();
 			fileIn.close();
 			return e;
@@ -98,8 +109,6 @@ public class Table implements Serializable {
 	}
 
 	public static void main(String[] args) {
-		
-		
 
 	}
 
